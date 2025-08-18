a8c6c612fcd7b64d263402cfc174b9f7253df5c0
ana <anna.cristinna.2008@gmail.com>
2025-08-18 11:58:29 -0300

    metodo post

--- Servidor/app.py
+++ Servidor/app.py
@@ -1,7 +1,19 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route('/api/sensor', methods=['POST'])
+def process_sensor():
+    data = request.json
+    nombre = data.get ('ana')
+    valor = data.get ('18')
+    print(f"jelou{nombre} y {valor}")
+    return 'ns'
+ #hacer q los datos se guarden el una base de datos en tiempo real con current_timestamp
+ #exportar el sql, la estructura
+
