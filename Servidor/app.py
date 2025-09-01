480e3f8d6bc53b3b3719fee3a49e629adc61b133
ana <anna.cristinna.2008@gmail.com>
2025-09-01 11:21:03 -0300

    ignorar el archivo sql

--- Servidor/app.py
+++ Servidor/app.py
@@ -1,19 +1,35 @@
-from flask import Flask
-from flask import request
+import sqlite3
+from flask import Flask, g, jsonify, request, url_for
+from math import ceil
+
+def dict_factory(cursor, row):
+  """Arma un diccionario con los valores de la fila."""
+  fields = [column[0] for column in cursor.description]
+  return {key: value for key, value in zip(fields, row)}
+
+def abrirConexion():
+   if 'db' not in g:
+      g.db = sqlite3.connect("sensores.sqlite")
+      g.db.row_factory = dict_factory
+   return g.db
+
+def cerrarConexion(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 app = Flask(__name__)
+app.teardown_appcontext(cerrarConexion)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/test")
+def test():
+    return "funcionando!"
 
-@app.route('/api/sensor', methods=['POST'])
-def process_sensor():
-    data = request.json
-    nombre = data.get ('ana')
-    valor = data.get ('18')
-    print(f"jelou{nombre} y {valor}")
-    return 'ns'
- #hacer q los datos se guarden el una base de datos en tiempo real con current_timestamp
- #exportar el sql, la estructura
+@app.route("/api/sensor", methods=['POST'])
+def sensor():
+    datos = request.json
+    nombre = datos["nombre"]
+    valor = datos["valor"]
+    print(f"nombre del sensor {nombre}, valor: {valor}")
+    return "OK"
 
